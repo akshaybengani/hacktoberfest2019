5cca905c8aa0ad24e6aa860a6f09fd23decebb85
PIYUSH AGARWAL <33891254+piyushagarwal08@users.noreply.github.com>
2019-10-03 19:25:51 +0530

    problem of life

---
+++ shitty_heart.py
@@ -0,0 +1,12 @@
+# Program to Make a heart
+import pyfiglet
+print(pyfiglet.figlet_format("I"))
+for i in range(6):
+    for j in range(7):
+        if (i==0 and j%3!=0) or (i==1 and j%3==0) or (i-j==2) or (i+j==8):
+            print('*',end=" ")
+        else:
+            print(end=" ")
+    print()
+print(pyfiglet.figlet_format("YOU"))
+print(pyfiglet.figlet_format("Python"))
