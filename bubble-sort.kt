deb794c7473d107f6548a92c7e82272be0d149f7
ironhead690 <44122186+ironhead690@users.noreply.github.com>
2019-10-16 16:26:00 +0300

    Create bubble-sort.kt

---
+++ bubble-sort.kt
@@ -0,0 +1,20 @@
+public class BubbleSort : SortAlgorithm{
+
+    public override fun sort(arr: Array<Long>): Array<Long> {
+        var swappedElements : Boolean;
+        do {
+            swappedElements = false;
+            for (i in 0..arr.size - 2){
+                if (arr[i] > arr[i + 1]){
+                    swap(arr, i, i + 1);
+                    swappedElements = true;
+                }
+            }
+        } while (swappedElements);
+        return arr;
+    }
+
+    public override fun getName(): String {
+        return "BubbleSort";
+    }
+}
